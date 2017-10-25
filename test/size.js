4727baf288610e88b400ca05a016618e792fca46
Alvaro Bernal <alvaro.bernal.gimeno@gmail.com>
2017-10-25 18:18:46 +0100

    Refactor

--- test/size.js
+++ test/size.js
@@ -6,11 +6,9 @@ test('--size kb', (t) => {
 	return new Promise((resolve, reject)=>{
 
 		exec('find test/data | node ./dist/total.js --size kb', (err, stdout, stderr) => {
-			if (err) return reject(t.fail())
-			if(stderr){
-				console.log(stderr)
-				reject()
-			}
+			if(err) return reject(err)
+			if(stderr) return reject(stderr)
+
 			t.true(between(64.00, 65.81)(Number(stdout)))
 			resolve()
 		})
@@ -21,12 +19,9 @@ test('--size b', (t) => {
 	return new Promise((resolve, reject)=>{
 
 		exec('find test/data | node ./dist/total.js --size b', (err, stdout, stderr) => {
-			if (err) return reject(t.fail())
-			if(stderr){
-				console.log(stderr)
-				t.fail()
-				reject()
-			}
+			if(err) return reject(err)
+			if(stderr) return reject(stderr)
+			
 			t.true(between(64000, 65010)(Number(stdout)))
 			resolve()
 		})
@@ -36,13 +31,9 @@ test('--size b', (t) => {
 test('--size', (t) => {
 	return new Promise((resolve, reject)=>{
 		exec('find test/data | node ./dist/total.js --size', (err, stdout, stderr) => {
-			if (err) return reject(t.fail())
-
-			if(stderr){
-				console.log('Failed: ', stderr)
-				t.fail()
-				reject()
-			}
+			if(err) return reject(err)
+			if(stderr) return reject(stderr)
+			
 
 			t.true(between(64800.000, 65010.000)(Number(stdout)))
 			resolve()
